ec9505e1315730d3d498fb60d8ef3440d45d789e
maegawa_h <hiroshi.maegawa.j6c@jpgr.denso.com>
2022-12-01 15:29:19 +0900

    initial commit

---
+++ src/TopApp/TopApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ChildLib;
+using GrandChildLib;
+
+namespace TopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("console.start");
+
+            var grandChild = new GrandChild();
+            grandChild.Call();
+
+            var child = new Child();
+            child.Call();
+
+            Console.WriteLine("console.end");
+        }
+    }
+}
